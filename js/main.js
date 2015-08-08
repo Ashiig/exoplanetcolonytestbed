aa93432f371cfe62e60d27f30502f0e8dbb41814
James Smith <js486b@gmail.com>
2015-08-08 13:09:51 -0400

    afafaf
    afafafa

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ window.onload = function() {
 	var game = new Phaser.Game(800,600,Phaser.AUTO, '', { preload: preload, create: create});
 
 	function preload() {
-		game.load.image('logo', 'assets/images/phaser.png');
+		game.load.image('bgtile', 'assets/images/bgtile.bmp');
 	}
 
 	function create () {
+	    var background = new TileSprite(game, 0,0, game.world.width, game.world.height, 'bgtile', 0);
             var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
             logo.anchor.setTo(0.5, 0.5);
 	}
