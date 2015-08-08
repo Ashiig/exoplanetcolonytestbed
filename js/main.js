a72803d2b85d6dd882d62ba0669e0a01aba5bdca
James Smith <js486b@gmail.com>
2015-08-08 12:48:43 -0400

    asdfas
    afsadsfas

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ window.onload = function() {
 	}
 
 	function create () {
-
+            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+            logo.anchor.setTo(0.5, 0.5);
 	}
 
 };
