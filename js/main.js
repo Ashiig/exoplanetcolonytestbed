0b01835cbd48438159d84a28c3456816be75dadd
James Smith <js486b@gmail.com>
2015-08-08 13:38:36 -0400

    asdafs
    asfasd

--- js/main.js
+++ js/main.js
@@ -4,12 +4,17 @@ window.onload = function() {
 
 	function preload() {
 		game.load.image('bgtile', 'assets/images/bgtile.bmp');
+		game.load.image('build001', 'assets/images/building001.bmp');
 	}
 
 	function create () {
 	    
             var logo = game.add.tileSprite(0, 0,800,640, 'bgtile',0);
-          
+            //var building = game.add.sprite(1,1,build001);
+	    var bmd = game.make.bitmapData(64,64);
+	    bmd.alphaMask('build001','build001');
+
+	    game.add.image(1,1,bmd);
 	}
 
 };
